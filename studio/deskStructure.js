0605f369420e33472dd972cb8c3293f68deccd1d
ladell.erby <ladellerby@techsavagery.net>
2021-09-11 02:17:41 -0700

    desktop strucure update

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,5 +1,5 @@
-import S from "@sanity/desk-tool/structure-builder";
- 
+import S from '@sanity/desk-tool/structure-builder';
+
 export default () =>
   S.list()
     .title('Field Reports')
@@ -10,16 +10,18 @@ export default () =>
           S.list()
             .title('General')
             .items([
-              S.document()
+              S.listItem()
+                .S.document()
                 .schemaType('reporter')
                 .documentId('reporter'),
-              S.document()
+              S.listItem()
+                .S.document()
                 .schemaType('produce')
-                .documentId('produce')
+                .documentId('produce'),
             ])
         ),
       // Add a visual divider (optional)
       S.divider(),
       // List out the rest of the document types, but filter out the config type
-      ...S.documentTypeListItems()
-    ])
+      ...S.documentTypeListItems(),
+    ]);
