650c91a149e06729aff84175ab4d16ea267f5f31
ladell.erby <ladellerby@techsavagery.net>
2021-09-12 23:11:00 -0700

    - create more grape reports component - adjust desk menu

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -4,14 +4,12 @@ export default () =>
   S.list()
     .title('Settings')
     .items([
-      S.listItem()
-      .title('Reporters')
-      .schemaType('reporter'),
-      S.listItem()
-      .title('Produce')
-      .schemaType('produce'),
+      S.listItem().title('Reporters').schemaType('reporter'),
+      S.listItem().title('Produce').schemaType('produce'),
       // Add a visual divider (optional)
       S.divider(),
       // List out the rest of the document types, but filter out the config type
-      ...S.documentTypeListItems(),
+      ...S.documentTypeListItems().filter(
+        (item) => !['produce', 'reporter'].includes(item.getId())
+      ),
     ]);
