0cd09d404dfa69d82fe1149e5bd03b34eed767c2
ladell.erby <ladellerby@techsavagery.net>
2021-09-12 23:26:37 -0700

    - change main page font size - update desk menu order

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -2,14 +2,18 @@ import S from '@sanity/desk-tool/structure-builder';
 
 export default () =>
   S.list()
-    .title('Settings')
+    .title('Reports')
     .items([
-      S.listItem().title('Reporters').schemaType('reporter'),
-      S.listItem().title('Produce').schemaType('produce'),
-      // Add a visual divider (optional)
+      S.listItem()
+        .title('Grape Reports')
+        .schemaType('grapeReport')
+        .documentTypeListItems(),
       S.divider(),
-      // List out the rest of the document types, but filter out the config type
-      ...S.documentTypeListItems().filter(
-        (item) => !['produce', 'reporter'].includes(item.getId())
-      ),
     ]);
+S.list()
+  .title('Settings')
+  .items([
+    ...S.documentTypeListItems().filter(
+      (item) => !['grapeReport', 'post'].includes(item.getId())
+    ),
+  ]);
