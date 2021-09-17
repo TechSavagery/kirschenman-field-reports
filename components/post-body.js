f37d299e74e1a740a0c447f4a56f59a86c30605c
ladell.erby <ladellerby@techsavagery.net>
2021-09-17 12:51:03 -0700

    - make unpopulted fields condtionally populated

--- components/post-body.js
+++ components/post-body.js
@@ -33,11 +33,11 @@ export default function PostBody({
           <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
             <div key={brix} className="border-t border-gray-200 pt-4">
               <dt className="font-medium text-gray-900">Lot</dt>
-              <dd className="mt-2 text-sm text-gray-500"></dd>
+              <dd className="mt-2 text-sm text-gray-500">{lot ? lot.name : "N/A"}</dd>
             </div>
             <div key={brix} className="border-t border-gray-200 pt-4">
               <dt className="font-medium text-gray-900">Block</dt>
-              <dd className="mt-2 text-sm text-gray-500"></dd>
+              <dd className="mt-2 text-sm text-gray-500">{blockNumber ? blockNumber : "xxxxxxx"}</dd>
             </div>
             <div key={brix} className="border-t border-gray-200 pt-4">
               <dt className="font-medium text-gray-900">Brix</dt>
