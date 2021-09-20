23434496419ff27799d9f170bc976b7b944bed86
ladell.erby <ladellerby@techsavagery.net>
2021-09-20 01:34:36 -0700

    - updating icon ref

--- components/dashboard-section-header.js
+++ components/dashboard-section-header.js
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 import {
   BriefcaseIcon,
   CalendarIcon,
-  Eye,
+  EyeIcon,
   ChevronDownIcon,
   CurrencyDollarIcon,
   LinkIcon,
@@ -48,7 +48,7 @@ export default function DashboardSectionHeader(title, link) {
               type="button"
               className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              <Eye className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+              <EyeIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
               View All
             </button>
           </a>
