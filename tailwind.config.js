42dd6e9132e5cf73c773558e24c9c8e556a37153
ladell.erby <ladellerby@techsavagery.net>
2021-09-22 01:48:59 -0700

    -adjust screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      'sm': '640px',
-
-      'md': '768px',
-
-      'lg': '1024px',
-
-      'xl': '1280px',
-
-      '2xl': '1536px',
-
-      '3xl': '2500px',
+      '3xl': '1920px',
     },
     extend: {
       colors: {
