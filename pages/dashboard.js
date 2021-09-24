96e41387b870b281f2652e9b417c48b1c3c19843
ladell.erby <ladellerby@techsavagery.net>
2021-09-24 01:25:04 -0700

    - remove excessive module import from dashboard.js - add params to GrapeReportsSection component

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,10 @@
 import Container from '../components/container';
 import MoreStories from '../components/more-stories';
 import HeroPost from '../components/hero-post';
-import Intro from '../components/intro';
 import Layout from '../components/layout';
 import { getAllPostsForHome } from '../lib/api';
 import Head from 'next/head';
-import HeroHeader from '../components/hero-header';
-import { CMS_NAME } from '../lib/constants';
 import PageHeader from '../components/page-header';
-import DashboardSectionHeader from '../components/dashboard-section-header';
 import GrapeReportsSection from '../components/grape-reports-section';
 import DashboardSectionHeader from '../components/dashboard-section-header';
 
