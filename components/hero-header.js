47860360ddc0d88b976e9d5c72c8051237b7cc8b
ladell.erby <ladellerby@techsavagery.net>
2021-09-24 00:53:57 -0700

    - Adding font for tailwind ui - adjusting screen widths for home page componenets

--- components/hero-header.js
+++ components/hero-header.js
@@ -9,8 +9,8 @@ const navigation = [
 
 export default function HeroHeader() {
   return (
-    <div className="relative bg-white overflow-hidden">
-      <div className="max-w-7xl mx-auto">
+    <div className="max-w-full relative bg-white overflow-hidden">
+      <div className="max-w-full mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-3xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
             className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
