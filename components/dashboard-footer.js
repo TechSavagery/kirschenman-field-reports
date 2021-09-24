7639fd3643e35abe0317cbe3cc258c2faec1a288
ladell.erby <ladellerby@techsavagery.net>
2021-09-24 10:50:20 -0700

    - add mail icon

--- components/dashboard-footer.js
+++ components/dashboard-footer.js
@@ -1,4 +1,4 @@
-import { PhoneIcon, EmailIcon } from '@heroicons/react/solid';
+import { PhoneIcon, MailIcon } from '@heroicons/react/solid';
 export default function DashboardFooter() {
   return (
     <div className="max-w-full bg-gray-50">
@@ -24,7 +24,7 @@ export default function DashboardFooter() {
               href="#"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
             >
-              <EmailIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+              <MailIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
               Email Us
             </a>
           </div>
