43ae14950178b10ea8223d1b36e31b85bf0b4e82
ladell.erby <ladellerby@techsavagery.net>
2021-09-24 00:42:29 -0700

    - adding back screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
       '3xl': '1920px',
     },
     extend: {
