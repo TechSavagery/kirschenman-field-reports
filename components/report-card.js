69be892aba43c79683764df6eaa65e356344a9c2
ladell.erby <ladellerby@techsavagery.net>
2021-09-24 02:53:03 -0700

    - update iamge reference in ReportCard component

--- components/report-card.js
+++ components/report-card.js
@@ -97,7 +97,7 @@ export default function ReportCard({
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
         <img
           src={imageBuilder(mainImage).width(280).height(320).url()}
-          alt={image.asset._ref}
+          alt={mainImage.asset._ref}
           className="w-full h-full object-center object-cover group-hover:opacity-75"
         />
       </div>
