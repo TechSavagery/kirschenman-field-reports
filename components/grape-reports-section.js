de973094c8c920cbdc9bd653ea1df1370bfbd8b5
ladell.erby <ladellerby@techsavagery.net>
2021-09-24 15:52:41 -0700

    - adjusting # of columns for grape reports section.

--- components/grape-reports-section.js
+++ components/grape-reports-section.js
@@ -90,7 +90,7 @@ export default function GrapeReportsSection({ reports }) {
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
 
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
           {reports.map((report) => (
             <ReportCard
               label={report.label}
