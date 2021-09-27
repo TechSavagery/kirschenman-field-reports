c80e7203edd9af2cc64dcf88d4489f661fe62edc
ladell.erby <ladellerby@techsavagery.net>
2021-09-27 01:16:41 -0700

    updating imports on report filter page

--- pages/dashboard/example-4.js
+++ pages/dashboard/example-4.js
@@ -31,6 +31,7 @@ import {
 } from '@heroicons/react/outline';
 import { ChevronDownIcon, PlusSmIcon } from '@heroicons/react/solid';
 import ReportCard from '../../components/report-card';
+import { getAllPostsForHome } from '../../lib/api';
 
 const navigation = {
   categories: [
