4ee17f41dfba6fcb0f44ee6b0a5919d1ee5da746
ladell.erby <ladellerby@techsavagery.net>
2021-09-28 00:18:29 -0700

    - report header update

--- components/report-header.js
+++ components/report-header.js
@@ -15,7 +15,7 @@ export default function ReportHeader({
   reporter,
 }) {
   return (
-    <main className="lg:relative">
+    <section className="lg:relative">
       <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
         <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
@@ -75,7 +75,7 @@ export default function ReportHeader({
           alt=""
         />
       </div>
-    </main>
+    </section>
   );
 }
 
