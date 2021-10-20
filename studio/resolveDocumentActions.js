c75489ca4d0ddbe32e0c2425878e3fca2f47514f
ladell.erby <ladellerby@techsavagery.net>
2021-10-20 10:06:18 -0700

    - update generateLinksAction reference

--- studio/resolveDocumentActions.js
+++ studio/resolveDocumentActions.js
@@ -1,5 +1,5 @@
 import defaultResolve from 'part:@sanity/base/document-actions'
-import {GenerateUrlsAction} from './generateUrlsAction'
+import {GenerateUrlsAction} from './GenerateUrlsAction'
 
 export default function resolveDocumentActions(props) {
   return [...defaultResolve(props), GenerateUrlsAction]
